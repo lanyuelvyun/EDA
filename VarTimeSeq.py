b1e8ed820be7dbb72be50657122191ce7a4b7fdd
lanyuelvyun <1969570533@qq.com>
2020-04-23 14:11:12 +0800

    Update VarTimeSeq.py

--- VarTimeSeq.py
+++ VarTimeSeq.py
@@ -41,12 +41,23 @@ class VarTimeSeq(object):
         df_quantile = self.get_var_quantile()
         df_coverage = self.get_var_coverage()
         df_odds = self.get_var_odds()
-        df_result = pd.concat([df_quantile, df_coverage, df_odds], axis=1, join='outer')  # 按照列进行拼接
-        df_result.rename(columns={0: "coverage", 1: "odds"}, inplace=True)
+        df_distr = self.get_var_distr()
+        df_result = pd.concat([df_quantile, df_coverage, df_odds, df_distr], axis=1, join='outer')  # 按照列进行拼接
+        df_result.rename(columns={0:"coverage", 1:"odds", 2:"distr"}, inplace=True)
 
-        df_result.to_csv(self.__save_path + '/%s_quantile_coverage_odds.csv' % self.__var_name, index=True,encoding="utf-8")
-        print(self.__save_path + '/%s_quantile_coverage_odds.csv' % self.__var_name)
+        df_result.to_csv(self.__save_path + '/%s_quantile_coverage_odds_distr.csv' % self.__var_name, index=True,encoding="utf-8")
+        print(self.__save_path + '/%s_quantile_coverage_odds_distr.csv' % self.__var_name)
         self.__plot(df_result)
+
+        
+    def get_var_distr(self):
+        """
+        变量频数分布图：对该变量进行分箱（等频+等宽），统计每一箱内的频数占比
+        """
+        print("get var distr".center(80, '*'))
+        df_copy = self.__df.copy()
+        df_distr = df_copy.groupby(self.__split_col).apply(lambda x: x.shape[0]*1.0 / (df_copy.shape[0]+1e-20))
+        return df_distr 
 
     def get_var_odds(self):
         """
@@ -76,7 +87,8 @@ class VarTimeSeq(object):
         """
         print("get var quantile".center(80, '*'))
         # 去掉空值
-        df_copy = self.__df[~self.__df[self.__var_name].isin(["NULL", "null", "", np.nan])].copy()
+        df_copy = self.__df.copy()
+        df_copy = df_copy[~df_copy[self.__var_name].isin(["NULL", "null", "", np.nan])]
         df_copy = df_copy[df_copy[self.__var_name].notnull()]  # 注意pandas 中的null，不是np.nan!!!
         # 计算每一分组内的分位数
         df_quantile = df_copy.groupby(self.__split_col).apply(lambda x: self.__cal_quantile(x)).reset_index(level=1, drop=True)
@@ -113,18 +125,19 @@ class VarTimeSeq(object):
                     for a, b in zip(X, df_result[i]):
                         plt.text(a, b, '%s' % (round(b, 3)), ha='center', va='bottom', fontsize=10) # plt.text 在曲线上显示y值
             else:
-            #     plt.plot(list(range(len(X))), df_result[col], label=col, color="green")
-            #     plt.xticks(list(range(len(X))), tuple(X), color='black', rotation=45) # 横坐标旋转60度
-                plt.plot(X, df_result[col], label=col, color="green")
-                plt.xticks(X, color='black', rotation=45) # 横坐标旋转60度
-                for a, b in zip(X, df_result[col]): 
-                    plt.text(a, b, '%s' % (round(b, 3)), ha='center', va='bottom', fontsize=10)  # plt.text 在曲线上显示y值    
+                for i in ["coverage","odds","distr"]:
+                #     plt.plot(list(range(len(X))), df_result[i], label=i, ior="green")
+                #     plt.xticks(list(range(len(X))), tuple(X), ior='black', rotation=45) # 横坐标旋转60度
+                    plt.plot(X, df_result[i], label=i)
+                    plt.xticks(X, tuple(X), color='black', rotation=45) # 横坐标旋转60度
+                    for a, b in zip(X, df_result[i]): 
+                        plt.text(a, b, '%s' % (round(b, 3)), ha='center', va='bottom', fontsize=10)  # plt.text 在曲线上显示y值    
             plt.grid(axis='y', color='#8f8f8f', linestyle='--', linewidth=1)  # 显示网格(如显示y轴的)
             plt.title('%s_%s' % (self.__var_name, col))
             plt.xlabel("%s" % self.__split_col)
             plt.ylabel(col)
             plt.legend()  # 用来显示图例
-        path = os.path.join(self.__save_path, self.__var_name+'_quantile_coverage_odds.png')
+        path = os.path.join(self.__save_path, self.__var_name+'_quantile_coverage_odds_distr.png')
         print("save_path is %s" % path)
         plt.savefig(path)
         plt.close()
